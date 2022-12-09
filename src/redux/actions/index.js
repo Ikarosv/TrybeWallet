55a6b7ae4703af78a5e44098d24df22a158ae46c
Ikarosv <ikarovieiraa@gmail.com>
2022-12-09 01:32:36 -0300

    feat: Adicionado action de requisição a api

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,22 @@
 // Coloque aqui suas actions
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SEARCH_CURRENCIES = 'SEARCH_CURRENCIES';
 
 export const userEmailSaver = (email) => ({
   type: SAVE_EMAIL,
   payload: email,
 });
+
+export const objKeys = (obj) => Object.keys(obj);
+
+export const searchCurrencies = (currencies) => ({
+  type: SEARCH_CURRENCIES,
+  payload: objKeys(currencies).filter((currency) => currency !== 'USDT'),
+});
+
+export const currencyApi = () => async (dispatch) => {
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((data) => dispatch(searchCurrencies(data)))
+    .catch((error) => console.log(error));
+};
