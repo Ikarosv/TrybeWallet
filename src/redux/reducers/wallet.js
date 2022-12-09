66527de32cd19572c2fbbaef998e29c8e350fb33
Ikarosv <ikarovieiraa@gmail.com>
2022-12-09 01:34:29 -0300

    feat: adicionado um novo case no switch

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,5 +1,7 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 
+import { SEARCH_CURRENCIES } from '../actions';
+
 const initialState = {
   currencies: [], // array de string
   expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
@@ -9,6 +11,11 @@ const initialState = {
 
 const wallet = (state = initialState, action) => {
   switch (action.type) {
+  case SEARCH_CURRENCIES:
+    return {
+      ...state,
+      currencies: action.payload,
+    };
   default: return state;
   }
 };
